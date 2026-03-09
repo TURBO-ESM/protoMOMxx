fd36080ec0cbd839a64a8e9f5cae326f5efc9f3b
alperaltuntas <alperaltuntas@gmail.com>
2026-03-09 14:10:40 -0600

    add a hello-world solo driver and CMake files

---
+++ config_src/drivers/solo_driver/MOM_driver.cpp
@@ -0,0 +1,19 @@
+/**
+ * @file MOM_driver.cpp
+ * @brief Driver program for protoMOMxx
+ */
+
+#include "MOM.h"
+#include "MOM_logger.h"
+
+/// @brief Main entry point for the protoMOMxx driver program.
+/// @return Exit code (0 for success, non-zero for failure)
+int main() {
+
+  MOM_logger::info("Hello C++ world. This is protoMOMxx!");
+
+  // Initialize the core MOM object
+  MOM mom;
+
+  return 0;
+}
