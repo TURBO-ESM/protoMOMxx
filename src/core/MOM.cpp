e138fd5e73a84942e1b7e0108d18be96da2e86c7
John Dennis <dennis@ucar.edu>
2026-03-19 13:45:20 -0600

    Added todo and defer functionality (#6)

--- src/core/MOM.cpp
+++ src/core/MOM.cpp
@@ -14,7 +14,43 @@ Model::Model(const int ensemble_num)
     logger::info("Ensemble number: ", ensemble_num_);
   }
 
-  // todo: Additional initialization code will be added here in the future.
+  // todo: Read namelist file
+  //
+  // todo: get_MOM_input():
+  //          	Gets the names of the I/O directories and initialization files
+  //          	Also calls the subroutine that opens run-time parameter files
+  //          	Needs namelist file capability
+  // todo: set_calendar_type()
+  //		Functionality from TIM/time_manager/time_manager.F90
+  // defer: time_interp_external_init()
+  // 		Initializes the interpolation capability in MOM
+  // todo: initialize_MOM()
+  //		Performs the core initialization of MOM
+  // todo: get_MOM_state_eleemnts()
+  // 		Functionality from src/core/MOM.F90
+  // defer: initialize_ice_shelf_fluxes()
+  // defer: initialize_ice_shelf_forces()	
+  // defer: ice_shelf_query()
+  // defer: data_override_init()
+  // todo: extract_surface_state()
+  // todo: surface_forcing_init()
+  // 		config_src/drivers/solo_dirver/MOM_surface_forcing.F90
+  // defer: MOM_wave_interface_init()
+  // 		src/user/MOM_wave_interface.F90
+  // todo: real_to_time()
+  // defer: diag_mediator_close_registration()
+  // defer: write_time_stamp_file()
+  // todo: set_forcing()
+  // 		MOM_surface_forcing.F90
+  // todo: finish_MOM_initilization()
+  // todo: step_MOM()
+  // 		src/core/MOM.F90
+  // defer: mech_forcing_diags()
+  // defer: forcing_diagnostics()
+  // defer: save_MOM_restart()
+  // defer: write_ocean_solo_res()
+  // defer: diag_mediator_end()
+  // todo: MOM_end()
 
 }
 
