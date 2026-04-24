b9811226218c740c78656416c04a908ccceec83c
Michael Waxmonsky <mwaxmonsky@ucar.edu>
2026-04-24 15:35:15 -0600

    Initial attempt to separate Derecho env from build and fix CI.

--- build_and_run.sh
+++ build_and_run.sh
@@ -9,11 +9,6 @@
 : ${AMREX_ROOT:="${ROOTDIR}/dependencies/amrex"}
 : ${FRESH_BUILD:="False"}
 
-# Current default modules on Derecho
-module purge
-module load ncarenv/25.10
-module load cmake/3.31.8 gcc/14.3.0 cray-mpich/8.1.32 ncarcompilers/1.2.0
-
 while [[ "$#" -gt 0 ]]; do
     case $1 in
         --gpu)
